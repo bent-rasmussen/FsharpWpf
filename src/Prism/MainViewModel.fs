2094f5041521e40f3cf84b0c62cfb7281300752e
Bent Rasmussen <bent.rasmussen@stati-cal.com>
2020-12-17 16:17:26 +0100

    Example prop with setter.

--- src/Prism/MainViewModel.fs
+++ src/Prism/MainViewModel.fs
@@ -5,4 +5,10 @@ open Statical.Prism
 type MainViewModel () =
     inherit ViewModelBase()
 
+    let mutable foo = 0
+
     member this.WorldName = "Elmish"
+
+    member this.PropWithSetter
+        with get () = foo
+        and set value = this.Set(&foo, value)
